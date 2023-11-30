6700e00b5ecc8113c0ef60ba4cfc885b1081daf2
Ronnie Smith <ronniesmith@outlook.com>
2023-11-30 13:23:34 +0000

    first commit

---
+++ hri_haru_action_bridge/src/main.py
@@ -0,0 +1,43 @@
+import rospy
+import actionlib
+from std_msgs.msg import String, Bool
+from idmind_tabletop_msgs.msg import TTSAction, TTSActionFeedback, TTSActionGoal, TTSActionResult, TTSGoal
+
+class HRIHaruActionBridge():
+    def __init__(self):
+        # init
+        rospy.init_node('hri_haru_action_bridge')
+
+        # ros handles
+        # haru
+        self.ros_ac_tts = actionlib.SimpleActionClient('/idmind_tabletop/action_tts', TTSAction)
+        # external
+        self.ros_sub_tts_goal = rospy.Subscriber('/hri_haru_action_bridge/tts/goal', String, callback=self.ros_cb_tts_goal)
+        self.ros_sub_tts_cancel = rospy.Subscriber('/hri_haru_action_bridge/tts/cancel', String, callback=self.ros_cb_tts_cancel)
+
+        # action server waits
+        self.ros_ac_tts.wait_for_server()
+
+    # haru interaction
+
+    def tts_goal(self, say):
+        goal = TTSGoal()
+        goal.message = str(say)
+        self.ros_ac_tts.send_goal(goal)
+
+    def tts_cancel(self):
+        self.ros_ac_tts.cancel_goal()
+        print(self.ros_ac_tts.get_goal_status_text())
+
+    # callbacks
+    def ros_cb_tts_goal(self, msg):
+        self.tts_goal(msg.data)
+    
+    def ros_cb_tts_cancel(self, msg):
+        self.tts_cancel()
+
+if __name__ == '__main__':
+    hhab = HRIHaruActionBridge()
+    hhab.tts_goal('Hello, my name is Haru')
+    rospy.sleep(1)
+    hhab.tts_cancel()
