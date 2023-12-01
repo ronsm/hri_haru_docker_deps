cf80af89eff49bdeaf07a93d88076215c98d5c59
Ronnie Smith <ronniesmith@outlook.com>
2023-12-01 14:35:59 +0000

    Added zenoh

--- hri_haru_action_bridge/src/main.py
+++ hri_haru_action_bridge/src/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import rospy
 import actionlib
+import zenoh
 from std_msgs.msg import String, Bool, Int32
 from idmind_tabletop_msgs.msg import TTSAction, TTSActionFeedback, TTSActionGoal, TTSActionResult, TTSGoal
 from idmind_tabletop_msgs.msg import RoutineAction, RoutineActionFeedback, RoutineActionGoal, RoutineActionResult, RoutineGoal
@@ -11,13 +12,14 @@ class HRIHaruActionBridge():
         rospy.init_node('hri_haru_action_bridge')
 
         # ros handles
-        # haru
         self.ros_ac_tts = actionlib.SimpleActionClient('/idmind_tabletop/action_tts', TTSAction)
         self.ros_ac_routine = actionlib.SimpleActionClient('/idmind_tabletop/action_routine', RoutineAction)
-        # external
-        self.ros_sub_tts_goal = rospy.Subscriber('/hri_haru_action_bridge/tts/goal', String, callback=self.ros_cb_tts_goal)
-        self.ros_sub_tts_cancel = rospy.Subscriber('/hri_haru_action_bridge/tts/cancel', String, callback=self.ros_cb_tts_cancel)
-        self.ros_sub_routine_goal = rospy.Subscriber('/hri_haru_action_bridge/routine/goal', Int32, callback=self.ros_cb_routine_goal)
+
+        # zenoh
+        self.zenoh_session = zenoh.open()
+        self.zenoh_sub_tts_goal = self.zenoh_session.declare_subscriber('hri_haru_action_bridge/tts/goal', self.zenoh_cb_tts_goal)
+        self.zenoh_sub_tts_cancel = self.zenoh_session.declare_subscriber('hri_haru_action_bridge/tts/cancel', self.zenoh_cb_tts_cancel)
+        self.zenoh_sub_routine_goal = self.zenoh_session.declare_subscriber('hri_haru_action_bridge/routine/goal', self.zenoh_cb_routine_goal)
 
         # action server waits
         self.ros_ac_tts.wait_for_server()
@@ -47,14 +49,20 @@ class HRIHaruActionBridge():
         print(self.ros_ac_routine.get_goal_status_text())
 
     # callbacks
-    def ros_cb_tts_goal(self, msg):
-        self.tts_goal(msg.data)
+    def zenoh_cb_tts_goal(self, msg):
+        self.tts_goal(msg.payload.decode('utf-8'))
     
-    def ros_cb_tts_cancel(self, msg):
+    def zenoh_cb_tts_cancel(self, msg):
         self.tts_cancel()
 
-    def ros_cb_routine_goal(self, msg):
-        self.routine_goal(msg.data)
+    def zenoh_cb_routine_goal(self, msg):
+        number = None
+        try:
+            number = int(msg.payload.decode('utf-8'))
+        except ValueError:
+            print('Invalid message received. Expected integer.')
+            return
+        self.routine_goal(msg.payload.decode('utf-8'))
 
 if __name__ == '__main__':
     hhab = HRIHaruActionBridge()
